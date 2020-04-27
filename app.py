789e904f9d191445c09678b5f7a5348290364056
blaxi <ooops4@gmail.com>
2020-04-27 11:18:26 +0530

    edited changed something

--- app.py
+++ app.py
@@ -21,11 +21,16 @@ jwt = JWTManager(app)
 CORS(app)
 
 
-#registration of users
+###########################################################################################
+########################            ADMIN API             #################################
+
+
+########################       REGISTRATION OF ADMIN      #################################
+
 @app.route('/api/register', methods=['POST'])
-def register():
+def registerAdmin():
     print(request.get_json())
-    users = mongo.db.users
+    users = mongo.db.admin
     first_name = request.get_json()['first_name']
     last_name = request.get_json()['last_name']
     email = request.get_json()['email']
@@ -69,11 +74,11 @@ def register():
     return jsonify({'result' : 'user saved'})
 	
 
-#login of user
 
+########################       LOGIN OF ADMIN      #################################
 @app.route('/api/login', methods=['POST'])
-def login():
-    users = mongo.db.users
+def loginAdmin():
+    users = mongo.db.admin
     email = request.get_json()['email']
     password = request.get_json()['password']
     result = ""
@@ -94,7 +99,101 @@ def login():
         result = jsonify({"result":"No results found"})
     return result
 
-#getting all the user
+
+
+###########################################################################################
+########################            DOCTOR API            #################################
+
+
+########################    REGISTRATION OF NEW DOCTOR    #################################
+@app.route('/api/doctor/register', methods=['POST'])
+def registerDoctor():
+    print(request.get_json())
+    users = mongo.db.doctors
+    first_name = request.get_json()['first_name']
+    last_name = request.get_json()['last_name']
+    email = request.get_json()['email']
+    password = bcrypt.generate_password_hash(request.get_json()['password']).decode('utf-8')
+    gender = request.get_json()['gender']
+
+    father_name = request.get_json()['father_name']
+    mother_name = request.get_json()['mother_name']
+    contact_number = request.get_json()['contact_number']
+    emergency_contact_number= request.get_json()['emergency_contact_number']
+    age = request.get_json()['age']
+    qualification = request.get_json()['qualification']
+    
+    created = datetime.utcnow()
+
+    user_id = users.insert({
+	'first_name' : first_name, 
+	'last_name' : last_name, 
+	'email' : email, 
+	'password' : password,
+    'age' : age,
+    'gender' : gender, 
+	'created' : created, 
+    'father_name':father_name,
+    'mother_name':mother_name,
+    'contact_number':contact_number,
+    'emergency_contact_number':emergency_contact_number,
+    'qualification':qualification
+
+
+
+    # 'address': {
+    #     city: city,
+    #     state:'Gujarat',
+        
+    # },
+	}),
+    print('user_id isisisisisi', user_id)
+    # new_user = users.find_one({'_id' : user_id})
+
+    # result = {'email' : new_user['email'] + ' registered'}
+    # print('result is ',result)
+    return jsonify({'result' : 'user saved'})
+	
+
+##################### LOGIN OF DOCTOR
+
+@app.route('/api/doctor/login', methods=['POST'])
+def loginDoctor():
+    users = mongo.db.doctors
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+    result = ""
+	
+    response = users.find_one({'email' : email})
+
+    if response:	
+        if bcrypt.check_password_hash(response['password'], password):
+            access_token = create_access_token(identity = {
+			    'first_name': response['first_name'],
+				'last_name': response['last_name'],
+				'email': response['email']}
+				)
+            result = jsonify({"token":access_token})
+        else:
+            result = jsonify({"error":"Invalid username and password"})            
+    else:
+        result = jsonify({"result":"No results found"})
+    return result
+
+
+##################### GETTING ALL THE DOCTORS ONLY NAMES ##########################
+@app.route('/api/doctor/users', methods=['GET'])
+def get_all_doctor_list():
+    user = mongo.db.doctors
+    result = []
+    for field in user.find():
+        result.append({'_id': str(field['_id']), 'first_name':field['first_name'], 'last_name':field['last_name']})
+    # *resp = dumps(users)
+    return jsonify(result)
+
+
+
+##################### GETTING ALL THE USERS ONLY NAMES ##########################
 @app.route('/api/users/', methods=['GET'])
 def get_all_users():
     user = mongo.db.users
@@ -129,7 +228,7 @@ def userss(id):
     # new_user = user.find_one({'_id': ObjectId(id)})
     # result = {'name': new_user['name']}
     return jsonify({'result': "Success"})
-
+# Deleting any user by ID
 @app.route('/api/user/<id>',methods=['DELETE'])
 def delete_user(id):
     user=mongo.db.user
@@ -142,7 +241,7 @@ def delete_user(id):
     return jsonify({'result': result})
 
 
-#chdsadsadasdsad
+#USER DETAILS WITH FULL DETAILS
 
 @app.route('/api/user/<id>',methods=['GET'])
 def view_details(id):
@@ -164,16 +263,27 @@ def view_details(id):
     return jsonify(output)
     
 
-@app.route('/api/user/cases/',methods=['GET'])
-def view_cases():
+@app.route('/api/user/cases/<id>',methods=['GET'])
+def view_cases(id):
     user = mongo.db.users
-    # result = []
-    result = user.find_one({"cases": {"$exists": {"case_name": True}}})
+    result = []
+    result = user.find_one({ 
+        "$and": [
+            {"_id": ObjectId(id)}, 
+            {"cases": {"$exists": {"case_name":True}}}
+        ] 
+        })
     # for field in user.find({'cases':"case_name"}):
         # result.append({'_id':ObjectId(id), 'case_name':field['case_name']})
     resp = json.loads(dumps(result))
     print(resp)
     return jsonify(resp)
+
+
+
+# @app.route('/api/cases/')
+#     cases= mongo.db.cases
+#     casenumb= request.get_json('')
 
 
     # user=mongo.db.user
@@ -189,7 +299,7 @@ def view_cases():
 def not_found(error=None):
     message = {
         'status':404,
-        'message':'Not found blah blah' + request.url
+        'message':'Not found blah blah NOT WORKING' + request.url
     }
     resp = jsonify(message)
 
